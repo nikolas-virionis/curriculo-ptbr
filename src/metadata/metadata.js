5af80401a6b4aeb34f475b0bb69d99edc5062114
Nikolas B Virionis <79850973+nikolas-virionis@users.noreply.github.com>
2023-06-12 12:38:14 -0300

    Initial commit

---
+++ src/metadata/metadata.js
@@ -0,0 +1,119 @@
+module.exports = {
+  name: 'Nikolas B Virionis',
+  title: 'Python Developer and Data Engineer',
+  facts: {
+    'Residence': '<a href="https://www.google.com/maps/place/R.+Tutóia,+701-900+-+Vila+Mariana,+São+Paulo+-+SP,+04007-004/@-23.578072,-46.6530841,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce5994b134d16b:0xf60aafdf8f1f0a55!8m2!3d-23.5780592!4d-46.6508468!16s%2Fg%2F1v4pv3q8?entry=ttu"><i class="fa fa-home fact-icon"></i>São Paulo, SP, Brazil</a>',
+    'Linkedin': `<a href="https://www.linkedin.com/in/nikolas-barcha-virionis/">
+                  <i class="fa-brands fa-linkedin fact-icon"></i>Nikolas B Virionis
+                </a>`,
+    'Github': `<a href="https://github.com/nikolas-virionis"><i class="fa-brands fa-github fact-icon"></i>nikolas-virionis</a>`,
+    'Email': '<a href="mailto:nikolas.virionis@gmail.com"><i class="fa fa-envelope fact-icon"></i>nikolas.virionis@gmail.com</a>'
+  },
+  skills: [
+    ['Python', 90],
+    ['PySpark', 80],
+    ['English', 80],
+    ['AWS', 75],
+    ['SQL', 75],
+    ['Pandas', 70],
+    ['Infrastructure as Code', 70],
+    ['IaC (AWS CDK)', 70]
+  ],
+  positions: [
+    {
+      title: 'Data Engineering Intern at yHub',
+      period: 'Jan/2022 ~ Present',
+      skills: ['Python', 'PySpark', 'AWS', 'SQL', 'Infrastructure as Code', 'AWS CDK'],
+      contents: `As a Data Engineering Intern at yHub, I gained invaluable hands-on experience in building cost-effective data pipelines using mostly serverless AWS Services. Collaborating closely with the team, I actively contributed to end-to-end development, problem-solving, and maintenance of the projects. Leveraging my skills in Python, Pandas, and PySpark, I implemented best practices to ensure efficient and scalable data processes. I proactively sought opportunities for improvement and successfully achieved three AWS Certifications in a few months of my professional career, allowing me to actively participate in solution discussions and make humble contributions to the team's success. Through my involvement in critical projects, I supported data-driven decision-making and enhanced my technical expertise while fostering effective collaboration within cross-functional teams.`
+    }
+  ],
+  educations: [
+    {
+      title: 'Bachelor of Computer Science at São Paulo Tech School',
+      period: 'Jan/2021 ~ Dec/2024',
+      skills: ['Python', 'PySpark', 'AWS', 'SQL', 'Javascript', 'Java', 'Microsoft Azure', 'Data Structures and Algorithms'],
+      contents: `Bachelor's degree in Computer Science with a strong emphasis on practical, real-world technologies and problem-solving skills. Gained valuable soft skills through dedicated "socio-emotional development" classes and acquired significant experience from engaging in extensive hands-on projects focused on real-world applications.`
+    },
+    {
+        title: 'High School at Colégio Dante Alighieri',
+        period: 'Jan/2018 ~ Dec/2020',
+        skills: ['Python', 'Problem Solving'],
+        contents: `I graduated as one of the top students in my class from one of São Paulo's most prestigious schools. During my academic journey, I had the valuable opportunity to kickstart my tech career by actively participating in elective classes focused on Python programming and problem solving.`
+    }
+  ],
+  certifications: [
+    {
+      title: 'AWS Certified Cloud Practitioner',
+      period: 'Apr/2022',
+      url: 'https://www.credly.com/badges/c0ffb338-5236-458a-a692-0441827ec7e3/linked_in_profile',
+    },
+    {
+      title: 'AWS Certified Developer Associate',
+      period: 'Aug/2022',
+      url: 'https://www.credly.com/badges/85e57612-1339-4013-89a7-390d8454cfed/linked_in_profile',
+    },
+    {
+      title: 'AWS Certified Solutions Architect Associate',
+      period: 'Nov/2022',
+      url: 'https://www.credly.com/badges/147e5d6e-3bf3-4de3-87e6-296d8006d2a4/linked_in_profile',
+    },
+  ],
+  courses: [
+    {
+      title: 'Taming Big Data with Apache Spark and Python, on Udemy',
+      period: 'Jan/2022',
+      skills: ['Python', 'PySpark'],
+      url: 'https://www.udemy.com/certificate/UC-7046784c-c39f-422d-8b31-ce783ce7cd41/'
+    },
+    {
+      title: 'Machine Learning, Data Science and Deep Learning with Python, on Udemy',
+      period: 'Jan/2022',
+      skills: ['Python', 'PySpark', 'Machine Learning', 'Pandas'],
+      url: 'https://www.udemy.com/certificate/UC-548dbf2e-1d88-4356-8c8a-9c7fb204e4cc/'
+    },
+    {
+      title: 'Cloud Computing 101, on AWS Educate',
+      period: 'Jan/2022',
+      skills: ['AWS'],
+      url: 'https://www.credly.com/badges/b378e9b5-337d-4878-a72f-f1ae300103a7/public_url'
+    },
+    {
+      title: 'Scala and Spark for Big Data and Machine Learning, on Udemy',
+      period: 'Jan/2023',
+      skills: ['Scala', 'Spark'],
+      url: 'https://www.udemy.com/certificate/UC-62bb400f-5e92-47e6-87f7-e92eb682e778/'
+    },
+  ],
+  experience: [
+    {
+      header: 'NPM published package',
+      skills: ['Machine Learning',  'Javascript'],
+      contents: `The linear-regression-model package provides a simpler way to get The linear regression and correlation between two variables in Node.`,
+      url: 'https://github.com/nikolas-virionis/linear-regression-model'
+    },
+    {
+      header: 'PyPI published package #1',
+      skills: ['Python', 'Machine Learning'],
+      contents: `The polynomial-regression-model package aims at from the data the user gives it, it deduces the best regression model to analyse it and returns the model, fit for predictions.`,
+      url: 'https://github.com/nikolas-virionis/polynomial-regression'
+    },
+    {
+      header: 'PyPI published package #2',
+      skills: ['Python', 'Machine Learning', 'Data Analysis', 'Working with external APIs'],
+      contents: `The spotify-recommender-api package serves the purpose of providing both recommendations for the user and a way for one to "cluster" automatically songs within a big playlist, base on the genres, popularity and song features, for example.`,
+      url: 'https://github.com/nikolas-virionis/spotify-api'
+    },
+    {
+      header: 'College project #1 - Safelog',
+      skills: ['Node', 'Java', 'AWS EC2'],
+      contents: `(aug/2021 - dec/2021) A Freshman year, group project in college, that collects machine health data and sets up alarms and dashboards based on that information.`,
+      url: 'https://github.com/nikolas-virionis/Safelog'
+    },
+    {
+      header: 'College project #2 - Homebox',
+      skills: ['React', 'Java Spring Boot', 'AWS'],
+      contents: `(jan/2022 - dec/2022) A group project in college, that provides an peer to peer business model platform (uber-like) around home services, such as plumbing or painting.`,
+      url: 'https://github.com/nikolas-virionis'
+    },
+  ]
+};
